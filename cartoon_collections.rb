d4193de269b50d4b766d9046d3db2faf11a2d7c6
Shahwar Spall <shevy.spall@gmail.com>
2019-07-23 02:28:35 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -3,8 +3,8 @@ def roll_call_dwarves(dwarves_array)
     dwarves_array.each_with_index {|dwarf, index| puts "#{index +1} #{dwarf}"}
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planet_calls)
+  planet_calls.map {|call| {
 end
 
 def long_planeteer_calls# code an argument here
