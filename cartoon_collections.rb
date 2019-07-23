02e73b4cfb6d42c8a0d58b51422ef0e2bc99b7cd
Shahwar Spall <shevy.spall@gmail.com>
2019-07-23 02:33:49 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -11,7 +11,7 @@ def long_planeteer_calls(calls_long)
   calls_long.any? {|call| call.size > 4}
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(cheddar_cheese)
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
